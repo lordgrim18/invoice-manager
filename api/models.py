7ca6dcc1b764d40743e7a988e00266614bedff01
lordgrim <jeromjomanthara@gmail.com>
2024-02-20 23:03:54 +0530

    fix(api): add sqllite table name in models

--- api/models.py
+++ api/models.py
@@ -7,11 +7,12 @@ class Invoice(models.Model):
     invoice_date = models.DateTimeField(auto_now_add=True)
     
     class Meta:
+        db_table = 'invoice'
         ordering = ['-invoice_date']
 
 class InvoiceDetail(models.Model):
     id = models.CharField(primary_key=True, max_length=36, default=uuid.uuid4, editable=False)
-    invoice = models.ForeignKey(Invoice, related_name='invoice details', on_delete=models.CASCADE)
+    invoice = models.ForeignKey(Invoice, related_name='invoice_details', on_delete=models.CASCADE)
     description = models.CharField(max_length=100, blank=False, null=False)
     quantity = models.IntegerField(blank=False, null=False)
     unit_price = models.DecimalField(max_digits=10, decimal_places=2, blank=False, null=False)
@@ -20,4 +21,5 @@ class InvoiceDetail(models.Model):
     updated_at = models.DateTimeField(auto_now=True)
     
     class Meta:
+        db_table = 'invoice_detail'
         ordering = ['-created_at', 'invoice']
