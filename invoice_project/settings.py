494c753c074b537866a20711ca09c2528fdcb19d
lordgrim <jeromjomanthara@gmail.com>
2024-02-20 19:14:35 +0530

    feat: add new app - api

--- invoice_project/settings.py
+++ invoice_project/settings.py
@@ -32,12 +32,16 @@ ALLOWED_HOSTS = config("ALLOWED_HOSTS", cast=lambda v: [s.strip() for s in v.spl
 # Application definition
 
 INSTALLED_APPS = [
-    'django.contrib.admin',
+    # 'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
+    # 'django.contrib.sessions',
+    # 'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+
+    # custom apps
+    'api',
 ]
 
 MIDDLEWARE = [
