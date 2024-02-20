2e7437a49700c6a3d099046908398479440bf223
lordgrim <jeromjomanthara@gmail.com>
2024-02-20 22:07:53 +0530

    feat(api): add invoice and invoice detail models

--- api/models.py
+++ api/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+import uuid 
 
-# Create your models here.
+class Invoice(models.Model):
+    id = models.CharField(primary_key=True, max_length=36, default=uuid.uuid4, editable=False)
+    customer_name = models.CharField(max_length=100, blank=False, null=False)
+    invoice_date = models.DateTimeField(auto_now_add=True)
+    
+    class Meta:
+        ordering = ['-invoice_date']
+
+class InvoiceDetail(models.Model):
+    id = models.CharField(primary_key=True, max_length=36, default=uuid.uuid4, editable=False)
+    invoice = models.ForeignKey(Invoice, related_name='invoice details', on_delete=models.CASCADE)
+    description = models.CharField(max_length=100, blank=False, null=False)
+    quantity = models.IntegerField(blank=False, null=False)
+    unit_price = models.DecimalField(max_digits=10, decimal_places=2, blank=False, null=False)
+    price = models.DecimalField(max_digits=10, decimal_places=2, blank=False, null=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
+    class Meta:
+        ordering = ['-created_at', 'invoice']
